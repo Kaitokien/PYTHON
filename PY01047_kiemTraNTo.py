aded67a936eb31fe40478336535d648175a447a5
Kaitokien <luutrungkien107@gmail.com>
2023-12-02 00:01:21 +0700

    Kiểm tra nguyên tố

---
+++ PY01047_kiemTraNTo.py
@@ -0,0 +1,19 @@
+from math import *
+
+def nt(n):
+    if n < 2:
+        return False
+    for i in range(2, isqrt(n) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    t = int(input())
+    while t > 0:
+        t -= 1
+        n = input()
+        tmp = n[len(n) - 4:len(n)]
+        if nt(int(tmp)) == True:
+            print("YES")
+        else: print("NO")
